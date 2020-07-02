4cccb6ca4453c69d4bf7a1828d11d72f03c6ddc3
unknown <trancuong.iot@gmail.com>
2020-07-02 17:09:22 +0700

    code 02/07

---
+++ src/main/java/cuong/in/demoRestGet/DemoRestGetApplication.java
@@ -0,0 +1,13 @@
+package cuong.in.demoRestGet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestGetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cuong/in/demoRestGet/DemoRestGetApplicationTests.java
@@ -0,0 +1,13 @@
+package cuong.in.demoRestGet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
