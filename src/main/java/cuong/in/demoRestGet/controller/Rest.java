87c1252c63574264db523af6c8ffd8d6742f944a
unknown <trancuong.iot@gmail.com>
2020-07-02 17:17:53 +0700

    update file Course

--- src/main/java/cuong/in/demoRestGet/controller/Rest.java
+++ src/main/java/cuong/in/demoRestGet/controller/Rest.java
@@ -19,13 +19,13 @@ public class Rest {
     private CourseConfiguration courseConfiguration;
 
 
-
+    //get data default from application.properties
     @RequestMapping("/defaultCourse")
     public  Course getDefaultCourse(@RequestParam(value="name", defaultValue = "Spring Boot", required= false ) String name,
                         @RequestParam(value = "chapterCount", defaultValue = "5", required = false) int chapterCount) {
         return  new Course(cName,chaptersCount);
     }
-
+    //get data default from application-qa.properties
     @RequestMapping("/get")
     public HashMap<String,Object> getConfig() {
         HashMap<String,Object> map = new HashMap<String,Object>();
@@ -37,13 +37,13 @@ public class Rest {
 
         return map;
     }
-
+    //Rest Get data
     @RequestMapping("/course")
     public  Course Rest(@RequestParam(value="name", defaultValue = "Spring Boot", required= false ) String name,
                         @RequestParam(value = "chapterCount", defaultValue = "5", required = false) int chapterCount) {
         return  new Course(name,chapterCount);
     }
-
+    //Rest Post
     @RequestMapping(method = RequestMethod.POST, value = "/register/course")
     public String saveCourse(@RequestBody Course course) {
         return "You add name " +course.getName() + " with " + course.getChapterCount() + " success";
